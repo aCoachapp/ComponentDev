c117a9d1e39c7208bacae1e63b882c213c338c8a
aCoachapp <norbert.gschvindt@digi.hu>
2025-08-09 08:42:35 +0200

    Add files via upload

---
+++ dist/input/editor.js
@@ -0,0 +1,9 @@
+import {
+  LineGraph
+} from '../../index.js'
+
+export const components = {
+  LineGraph
+}
+
+export const config = {"displayName":"aCoach Components","author":"","description":"","logo":"./example-logo.png","name":"acoach-components","version":"dev","components":[{"name":"LineGraph","displayName":"LineGraph","icon":"./example-thumbnail.png","defaultWidth":160,"defaultHeight":24,"props":[{"name":"text","displayName":"Text","type":"text","default":"Happy Hacking"},{"name":"color","displayName":"Color","type":"color","default":"#00A898"}]}]}
